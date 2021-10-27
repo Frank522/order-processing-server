a00412508bdee9e34dbb2f8e5fce2cd8686ab988
enyu <enyu@enyus-MacBook-Pro.local>
2021-10-26 21:11:57 -0400

    Try to fix

--- server.js
+++ server.js
@@ -21,11 +21,18 @@ const client = new Client({
 
 client.connect();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(function (req, res, next){
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  // res.setHeader('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Origin', '*');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 
 const ShippingQuery =
   "INSERT INTO shippinginfo VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);";
