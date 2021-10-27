4185133480ff436dd8ae2df28e4a16e8e756f931
enyu <enyu@enyus-MacBook-Pro.local>
2021-10-26 21:16:13 -0400

    Try to fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next){
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.header('Access-Control-Allow-Origin', '*');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
