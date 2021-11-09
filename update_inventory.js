07b559a141c40fe45f9cfdf868495a41fae38046
bpolito <bpolito88@yahoo.com>
2021-11-08 22:02:48 -0500

    udpated batch script

--- update_inventory.js
+++ update_inventory.js
@@ -9,37 +9,36 @@ const client = new Client({
     },
   });
 client.connect(); 
+let data = [];
 client.query(
         'SELECT po.plant_id, SUM(po.quantity_purchased) ' + 
         'FROM plant_orders po ' + 
-        //'LEFT JOIN "orders" "o" ON po.order_id = o.id' +
-        //'WHERE o.order_date > NOW() - INTERVAL \'24 HOURS\'' +
+        'LEFT JOIN "orders" "o" ON po.order_id = o.id ' +
+        'WHERE o.order_date > NOW() - INTERVAL \'24 HOURS\' ' +
         'GROUP BY "po".plant_id;',
         (err, res) => {
             if (err) throw err;
             for (let row of res.rows) {
-              console.log(JSON.stringify(row));
+              data.push({id: row.plant_id, quantity: row.sum});
             }
+            axios({
+              method: 'post',
+              url: 'https://cse5234-inventory-microservice.herokuapp.com/InventoryMicroservice/Update',
+              headers: { "Content-Type": "application/json" },
+              data: {
+                      products: data
+                  }
+          })
+          .then(function (response) {
+              //handle success, exit afterward
+              console.log(response);
+              process.exit();
+            })
+            .catch(function (response) {
+              //handle error, exit afterward
+              console.log(response);
+              process.exit();
+            });
           });
-
-axios({
-    method: 'post',
-    url: 'https://cse5234-inventory-microservice.herokuapp.com/InventoryMicroservice/Update',
-    headers: { "Content-Type": "application/json" },
-    data: {
-            products: []
-        }
-})
-.then(function (response) {
-    //handle success, exit afterward
-    console.log(response);
-    process.exit();
-  })
-  .catch(function (response) {
-    //handle error, exit afterward
-    console.log(response);
-    process.exit();
-  });
-
 // timeout if the call doesn't finish in 20 seconds
 setTimeout(() => {process.exit()}, 20000);
